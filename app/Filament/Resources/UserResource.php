fe9d2e7a8fe0f9deaaac306a41e5a33b2090a65c
Wellington Cavalcante <cgti@garanhuns.ifpe.edu.br>
2025-03-11 10:39:58 -0300

    Corrige o grupo de navegação de Usuários de Seguranças para Segurança

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -25,7 +25,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationLabel = 'Usuários';
 
-    protected static ?string $navigationGroup = 'Seguranças';
+    protected static ?string $navigationGroup = 'Segurança';
 
 
 
