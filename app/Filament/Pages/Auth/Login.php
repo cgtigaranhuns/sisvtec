1c5bacbe26b5d7146f53155133952a401d82c71d
Wellington Cavalcante <cgti@garanhuns.ifpe.edu.br>
2025-04-04 15:38:06 -0300

    Adiciona melhorias na página de login, incluindo cabeçalho e mensagens de validação personalizadas, além de ajustes na autenticação e configuração do painel administrativo.

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -7,9 +7,22 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Component;
+use Illuminate\Validation\ValidationException;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
 
 class Login extends BaseLogin
 {
+    public function getHeading(): string|\Illuminate\Support\HtmlString
+    {
+        return 'SISVTEC';
+    }
+
+    public function getSubheading(): string|\Illuminate\Support\HtmlString
+    {
+        return 'Sistema de Visitas Técnicas';
+    }
+
     public function form(Form $form): Form
     {
         return $form
@@ -23,18 +36,25 @@ class Login extends BaseLogin
                 ->afterStateUpdated(function ($state, callable $set) {
                     $connection = strlen($state) < 11 ? 'adm' : 'labs';
                     $set('ldap_connection', $connection);
-                }),
+                })->validationMessages([
+                    'required' => 'O campo usuário é obrigatório',
+                ]),
             
             TextInput::make('password')
                 ->label('Senha')
                 ->password()
-                ->required(),
+                ->revealable()
+                ->required()
+                ->validationMessages([
+                    'required' => 'O campo senha é obrigatório',
+                ]),
             
             Hidden::make('ldap_connection') // Campo oculto
                 ->default('adm'), // Valor padrão
             
             $this->getRememberFormComponent(),
-        ]);
+        ])
+        ->statePath('data');
     }
 
     protected function getCredentialsFromFormData(array $data): array
@@ -44,5 +64,27 @@ class Login extends BaseLogin
             'password' => $data['password'],
             'connection' => $data['ldap_connection'], // Adiciona a conexão selecionada
         ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.username' => __('filament-panels::pages/auth/login.messages.failed'),
+            'data.password' => __('filament-panels::pages/auth/login.messages.failed'),
+        ]);
+    }
+
+    public function authenticate(): ?LoginResponse
+    {
+        try {
+            return parent::authenticate();
+        } catch (ValidationException $e) {
+            session()->flash(
+                'errors',
+                array_merge(session()->get('errors', []), $e->errors())
+            );
+
+            throw $e;
+        }
     }
 }
