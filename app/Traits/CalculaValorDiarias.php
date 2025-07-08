ce30c1689b3f3d8f71d339bef720458cac51f3d6
Wellington Cavalcante <cgti@garanhuns.ifpe.edu.br>
2025-07-08 13:46:50 -0300

    trait

--- app/Traits/CalculaValorDiarias.php
+++ app/Traits/CalculaValorDiarias.php
@@ -32,6 +32,6 @@ trait CalculaValorDiarias
         }
 
         $set('valor_total_diarias', $valorDiarias);
-        $set('custo_total', ($valorDiarias + $get('menor_valor_hospedagem')));
+        $set('custo_total', ($valorDiarias + $get('menor_valor_hospedagem') + $get('menor_valor_passagens')));
     }
 }
